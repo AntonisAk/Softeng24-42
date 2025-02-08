149a7090354a0002d37fa39ca497333115f29d26
ntua-el21152 <el21152@mail.ntua.gr>
2025-02-08 17:25:05 +0200

    Map title & rounded corners

--- front-end/src/pages/Map.jsx
+++ front-end/src/pages/Map.jsx
@@ -31,7 +31,7 @@ const otherOperatorIcon = new L.Icon({
   shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
 });
 
-export default function Map() {
+function Map() {
   const { auth } = useContext(AuthContext);
   const [tollStations, setTollStations] = useState([]);
 
@@ -53,7 +53,7 @@ export default function Map() {
               name: station.Name,
               latitude: station.Lat,
               longitude: station.Long,
-              totalPasses: incomingPassData ? incomingPassData.totalpasses : 0, 
+              totalPasses: incomingPassData ? incomingPassData.totalpasses : 0,
               totalRevenue: incomingPassData ? incomingPassData.totalrevenue : 0,
               outgoingPasses: outgoingPassData ? outgoingPassData.totalpasses : 0,
               outgoingRevenue: outgoingPassData ? outgoingPassData.totalrevenue : 0,
@@ -74,46 +74,51 @@ export default function Map() {
   }, [auth.token]);
 
   return (
-    <div className={styles.mapContainer}>
-      <MapContainer
-        center={[39.0742, 21.8243]} 
-        zoom={7}
-        minZoom={6}
-        maxBounds={[[41.7489, 26.6042], [34.7001, 19.3722]]} 
-        maxBoundsViscosity={1.0}
-        style={{ height: "100vh", width: "100%" }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
+    <div className={styles.map}>
+      <h1>Toll Stations</h1>
+      <div className={styles.mapContainer}>
+        <MapContainer
+          center={[39.0742, 21.8243]}
+          zoom={7}
+          minZoom={6}
+          maxBounds={[[41.7489, 26.6042], [34.7001, 19.3722]]}
+          maxBoundsViscosity={1.0}
+          style={{ height: "100vh", width: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
 
-        {/* Render markers for each toll station */}
-        {tollStations.map((station, idx) => (
-          <Marker 
-            key={station.id || idx} 
-            position={[station.latitude, station.longitude]} 
-            icon={station.user_opid === station.ts_opid ? userOperatorIcon : otherOperatorIcon}
-          >
-            <Popup>
-              <strong>{station.name}</strong><br />
-              
-              {/* Show Operator Passes & Revenue only if the user belongs to this toll station's operator */}
-              {station.user_opid === station.ts_opid ? (
-                <>
-                  Passes: {station.totalPasses} <br />
-                  Revenue: €{station.totalRevenue} <br />
-                </>
-              ) : (
-                <>
-                  {station.ts_opname} Passes: {station.outgoingPasses} <br />
-                  {station.ts_opname} Revenue: €{station.outgoingRevenue} <br />
-                </>
-              )}
-            </Popup>
-          </Marker>
-        ))}
-      </MapContainer>
+          {/* Render markers for each toll station */}
+          {tollStations.map((station, idx) => (
+            <Marker
+              key={station.id || idx}
+              position={[station.latitude, station.longitude]}
+              icon={station.user_opid === station.ts_opid ? userOperatorIcon : otherOperatorIcon}
+            >
+              <Popup>
+                <strong>{station.name}</strong><br />
+
+                {/* Show Operator Passes & Revenue only if the user belongs to this toll station's operator */}
+                {station.user_opid === station.ts_opid ? (
+                  <>
+                    Passes: {station.totalPasses} <br />
+                    Revenue: €{station.totalRevenue} <br />
+                  </>
+                ) : (
+                  <>
+                    {station.ts_opname} Passes: {station.outgoingPasses} <br />
+                    {station.ts_opname} Revenue: €{station.outgoingRevenue} <br />
+                  </>
+                )}
+              </Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+      </div>
     </div>
   );
 }
+
+export default Map;
